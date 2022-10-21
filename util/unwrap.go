26806cf2296ab4fa22006b48bd6bc3d0eb5bf2e9
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2022-10-21 11:47:28 -0300

    fiz OT

--- util/unwrap.go
+++ util/unwrap.go
@@ -1,12 +1,13 @@
 package util
 
-func UnwrapPtr[IT any, OT any](fn func(input *IT) *OT) func(input *IT, err error) (*OT, error) {
-	return func(input *IT, err error) (*OT, error) {
+func UnwrapPtr[IT any, OT any](fn func(input *IT) OT) func(input *IT, err error) (OT, error) {
+	var zv OT
+	return func(input *IT, err error) (OT, error) {
 		if err != nil {
-			return nil, err
+			return zv, err
 		}
 		if input == nil {
-			return nil, nil
+			return zv, nil
 		}
 		return fn(input), nil
 	}
