e844898ec8b037cef98d0d73a1bf55e28bae2808
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2022-12-15 11:50:06 -0300

    extract content type

--- web/download.go
+++ web/download.go
@@ -7,6 +7,14 @@ import (
 	"net/http"
 )
 
+type ExtractContentTypeFunc func(contentType string)
+
+type ContextVar string
+
+const (
+	ContextVarExtractContentType ContextVar = "extract_content_type"
+)
+
 // DownloadInto downloads a file from a public URL
 func DownloadInto(ctx context.Context, url string, w io.Writer) (int64, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
@@ -20,6 +28,11 @@ func DownloadInto(ctx context.Context, url string, w io.Writer) (int64, error) {
 		return 0, fmt.Errorf("failed to download: %w", err)
 	}
 	defer resp.Body.Close()
+	if vi := ctx.Value(ContextVarExtractContentType); vi != nil {
+		if vf, ok := vi.(ExtractContentTypeFunc); ok {
+			vf(resp.Header.Get("Content-Type"))
+		}
+	}
 	if resp.StatusCode != http.StatusOK {
 		return 0, fmt.Errorf("failed to download: %s", resp.Status)
 	}
