2a2773ee5caa817b01fc806ef79022554e591f90
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2023-01-09 05:46:39 -0300

    add filter functions

--- elasticsearch/query.go
+++ elasticsearch/query.go
@@ -52,11 +52,33 @@ func BoolMustMatch[T any](q *Query, fieldName string, value T) {
 	})
 }
 
+func BoolFilterMatch[T any](q *Query, fieldName string, value T) {
+	if q.Bool.Filter == nil {
+		q.Bool.Filter = []map[string]any{}
+	}
+	q.Bool.Filter = append(q.Bool.Filter, map[string]any{
+		"match": map[string]any{
+			fieldName: value,
+		},
+	})
+}
+
 func BoolMustTerm[T any](q *Query, fieldName string, value T) {
 	if q.Bool.Must == nil {
 		q.Bool.Must = []map[string]any{}
 	}
 	q.Bool.Must = append(q.Bool.Must, map[string]any{
+		"term": map[string]any{
+			fieldName: value,
+		},
+	})
+}
+
+func BoolFilterTerm[T any](q *Query, fieldName string, value T) {
+	if q.Bool.Filter == nil {
+		q.Bool.Filter = []map[string]any{}
+	}
+	q.Bool.Filter = append(q.Bool.Filter, map[string]any{
 		"term": map[string]any{
 			fieldName: value,
 		},
@@ -82,6 +104,25 @@ func BoolMustRange[T any](q *Query, fieldName string, rng Range) {
 	})
 }
 
+func BoolFilterRange[T any](q *Query, fieldName string, rng Range) {
+	if q.Bool.Filter == nil {
+		q.Bool.Filter = []map[string]any{}
+	}
+	for bi := range q.Bool.Filter {
+		if rv := q.Bool.Filter[bi]["range"]; rv != nil {
+			rngg := rv.(map[string]Range)
+			rngg[fieldName] = rng
+			q.Bool.Filter[bi]["range"] = rngg
+			return
+		}
+	}
+	rnp := make(map[string]Range)
+	rnp[fieldName] = rng
+	q.Bool.Filter = append(q.Bool.Filter, map[string]any{
+		"range": rnp,
+	})
+}
+
 func BoolMustRanges[T any](q *Query, ranges map[string]Range) {
 	if q.Bool.Must == nil {
 		q.Bool.Must = []map[string]any{}
@@ -97,6 +138,25 @@ func BoolMustRanges[T any](q *Query, ranges map[string]Range) {
 		}
 	}
 	q.Bool.Must = append(q.Bool.Must, map[string]any{
+		"range": ranges,
+	})
+}
+
+func BoolFilterRanges[T any](q *Query, ranges map[string]Range) {
+	if q.Bool.Filter == nil {
+		q.Bool.Filter = []map[string]any{}
+	}
+	for bi := range q.Bool.Filter {
+		if rv := q.Bool.Filter[bi]["range"]; rv != nil {
+			rngg := rv.(map[string]Range)
+			for rk, rv := range ranges {
+				rngg[rk] = rv
+			}
+			q.Bool.Filter[bi]["range"] = rngg
+			return
+		}
+	}
+	q.Bool.Filter = append(q.Bool.Filter, map[string]any{
 		"range": ranges,
 	})
 }
