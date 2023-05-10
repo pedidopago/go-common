aaa2d5db006c81b8aa771c4144e84225c7ee8e8c
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2023-05-10 13:19:01 -0300

    added Deref

--- util/pointers.go
+++ util/pointers.go
@@ -4,3 +4,10 @@ func StringPtr(v string) *string {
 	v2 := v
 	return &v2
 }
+
+// Deref will dereference a generic pointer.
+// It will fail at compile time if ptr is not a pointer.
+// This is useful for functions that require pointers to structs and not structs directly.
+func Deref[T any](ptr *T) T {
+	return *ptr
+}
