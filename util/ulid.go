93c8df77d3e3e11f4e8672719629371252ae9fbf
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2023-06-23 15:22:52 -0300

    add ulid validation

--- util/ulid.go
+++ util/ulid.go
@@ -19,6 +19,15 @@ func SecureID() ulid.ULID {
 	return ulid.MustNew(ulid.Timestamp(time.Now()), secureSource)
 }
 
+// StringIsULID returns true if the string is a valid ULID
+func StringIsULID(s string) bool {
+	if len(s) != 26 {
+		return false
+	}
+	_, err := ulid.Parse(s)
+	return err == nil
+}
+
 func init() {
 	secureSource = ulid.Monotonic(srand.Reader, 0)
 }
