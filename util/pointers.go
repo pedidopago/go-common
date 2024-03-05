4d3917e7836ad2470c9d821dda852f10b497fac9
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2024-03-05 15:28:30 -0300

    implement safe defref

--- util/pointers.go
+++ util/pointers.go
@@ -12,6 +12,17 @@ func Deref[T any](ptr *T) T {
 	return *ptr
 }
 
+// SafeDeref will dereference a generic pointer.
+// It will fail at compile time if ptr is not a pointer.
+// The difference with Deref is that it will return the zero value of T if ptr is nil.
+func SafeDeref[T any](ptr *T) T {
+	if ptr == nil {
+		var zv T
+		return zv
+	}
+	return *ptr
+}
+
 func Ref[T any](v T) *T {
 	return &v
 }
