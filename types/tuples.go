ce3775c2f734a33ea784b413368d9ed62e9e5db5
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2025-02-17 09:10:55 -0300

    tuples

---
+++ types/tuples.go
@@ -0,0 +1,27 @@
+package types
+
+type Tuple2[T1, T2 any] struct {
+	T1 T1
+	T2 T2
+}
+
+type Tuple3[T1, T2, T3 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+}
+
+type Tuple4[T1, T2, T3, T4 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+}
+
+type Tuple5[T1, T2, T3, T4, T5 any] struct {
+	T1 T1
+	T2 T2
+	T3 T3
+	T4 T4
+	T5 T5
+}
