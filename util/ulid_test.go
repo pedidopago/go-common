092e63d43754c3106b5d4e2aac73024855a5430d
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2025-09-12 14:07:19 -0300

    refactor ulid_test to use channel for UUID management, improving concurrency and handling duplicate checks efficiently

--- util/ulid_test.go
+++ util/ulid_test.go
@@ -3,45 +3,57 @@ package util
 import (
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/oklog/ulid/v2"
 )
 
 func TestSecureID(t *testing.T) {
+	uuidchan := make(chan ulid.ULID, 10000)
 	allUUIDs := make(map[ulid.ULID]bool)
 	var allUUIDsLock sync.RWMutex
 
-	setnx := func(id ulid.ULID) (success bool) {
-		allUUIDsLock.RLock()
-		_, ok := allUUIDs[id]
-		allUUIDsLock.RUnlock()
-		if ok {
-			return false
-		}
-
-		allUUIDsLock.Lock()
-		defer allUUIDsLock.Unlock()
-
-		allUUIDs[id] = true
-		return true
+	addid := func(id ulid.ULID) {
+		uuidchan <- id
 	}
 
 	wg := &sync.WaitGroup{}
 
 	for i := 0; i < 5000; i++ {
 		wg.Add(1)
-		go randosUUIDs(t, setnx, 100, wg)
+		go randosUUIDs(t, addid, 100, wg)
 	}
 
+	go func() {
+		for id := range uuidchan {
+			allUUIDsLock.RLock()
+			_, ok := allUUIDs[id]
+			allUUIDsLock.RUnlock()
+			if ok {
+				t.Errorf("duplicate UUID: %s", id)
+			} else {
+				allUUIDsLock.Lock()
+				allUUIDs[id] = true
+				allUUIDsLock.Unlock()
+			}
+		}
+	}()
+
 	wg.Wait()
+
+	time.Sleep(time.Millisecond * 100)
+
+	close(uuidchan)
+
+	if len(allUUIDs) != 5000*100 {
+		t.Errorf("expected 5000*100 UUIDs, got %d", len(allUUIDs))
+	}
 }
 
-func randosUUIDs(t *testing.T, setnx func(ulid.ULID) bool, n int, wg *sync.WaitGroup) {
+func randosUUIDs(t *testing.T, addid func(ulid.ULID), n int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 0; i < n; i++ {
 		id := SecureID()
-		if !setnx(id) {
-			t.Errorf("duplicate id %s", id.String())
-		}
+		addid(id)
 	}
 }
